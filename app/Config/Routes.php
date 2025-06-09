32a6b8eb5e5c84b7ec0a7a1d886fec154a8f81eb
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2025-06-09 21:09:52 +0300

    show_posts

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,4 +6,9 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
 $routes->get('/users', 'Home::show_users');
+$routes->get('/users/(:num)', 'Home::show_users/$1');
+
+
+$routes->get('/posts', 'Home::show_posts');
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -17,48 +17,85 @@ class Home extends BaseController
         //return view('welcome_message');
     }
 
-     public function show_users()
+     public function show_users($userId = "")
      {
        $users = [
-    [
-        'name' => 'Jānis',
-        'surname' => 'Ozoliņš',
-        'email' => 'janis.ozolins@example.com',
-        'password' => password_hash('Parole123', PASSWORD_DEFAULT),
-        'address' => 'Brīvības iela 1, Rīga, LV-1010'
-    ],
-    [
-        'name' => 'Laura',
-        'surname' => 'Liepa',
-        'email' => 'laura.liepa@example.com',
-        'password' => password_hash('ManaParole!', PASSWORD_DEFAULT),
-        'address' => 'Skolas iela 15, Jelgava, LV-3001'
-    ],
-    [
-        'name' => 'Mārtiņš',
-        'surname' => 'Bērziņš',
-        'email' => 'martins.berzins@example.com',
-        'password' => password_hash('CitsParole2024', PASSWORD_DEFAULT),
-        'address' => 'Saulkrastu iela 23, Liepāja, LV-3401'
-    ],
-    [
-        'name' => 'Elīna',
-        'surname' => 'Kalniņa',
-        'email' => 'elina.kalnina@example.com',
-        'password' => password_hash('DrošaParole', PASSWORD_DEFAULT),
-        'address' => 'Pils iela 8, Valmiera, LV-4201'
-    ],
-    [
-        'name' => 'Andris',
-        'surname' => 'Zariņš',
-        'email' => 'andris.zarins@example.com',
-        'password' => password_hash('Super123!', PASSWORD_DEFAULT),
-        'address' => 'Dzirnavu iela 4, Daugavpils, LV-5401'
-    ]
+            [
+                'id'=>1,
+                'name' => 'Jānis',
+                'surname' => 'Ozoliņš',
+                'email' => 'janis.ozolins@example.com',
+                'password' => password_hash('Parole123', PASSWORD_DEFAULT),
+                'address' => 'Brīvības iela 1, Rīga, LV-1010'
+            ],
+            [
+                'id'=>2,
+                'name' => 'Laura',
+                'surname' => 'Liepa',
+                'email' => 'laura.liepa@example.com',
+                'password' => password_hash('ManaParole!', PASSWORD_DEFAULT),
+                'address' => 'Skolas iela 15, Jelgava, LV-3001'
+            ],
+            [
+                'id'=>3,
+                'name' => 'Mārtiņš',
+                'surname' => 'Bērziņš',
+                'email' => 'martins.berzins@example.com',
+                'password' => password_hash('CitsParole2024', PASSWORD_DEFAULT),
+                'address' => 'Saulkrastu iela 23, Liepāja, LV-3401'
+            ],
+            [
+                'id'=>4,
+                'name' => 'Elīna',
+                'surname' => 'Kalniņa',
+                'email' => 'elina.kalnina@example.com',
+                'password' => password_hash('DrošaParole', PASSWORD_DEFAULT),
+                'address' => 'Pils iela 8, Valmiera, LV-4201'
+            ],
+            [
+                'id'=>5,
+                'name' => 'Andris',
+                'surname' => 'Zariņš',
+                'email' => 'andris.zarins@example.com',
+                'password' => password_hash('Super123!', PASSWORD_DEFAULT),
+                'address' => 'Dzirnavu iela 4, Daugavpils, LV-5401'
+            ]
 ];
 
 
-        return $this->response->setJSON($users);
+        if($userId==""){
+            return $this->response->setJSON($users);
+        }
+        else{
+            $filtered_users = array_filter($users,function($item)  use ($userId){
+                if($item["id"]==$userId){
+                    return $item;
+                }
+
+            });
+            $user = array_values($filtered_users)[0] ?? "";
+
+            return $this->response->setJSON($user);
+
+        }
+        
+
+     }
+
+     public function show_posts(){
+            $posts = [];
+
+            $db      = \Config\Database::connect();
+            $builder = $db->table('posts');
+
+            $query   = $builder->get();
+
+            foreach ($query->getResult() as $row) {
+               $posts[] = $row;
+            }
+
+            return $this->response->setJSON($posts);
+
 
      }
 
